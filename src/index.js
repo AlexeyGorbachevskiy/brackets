e012d7531765a80d1ef2d5e8303edeb15e1db3c4
AlexeyGorbachevskiy <alexeygorbachevskiyy@gmail.com>
2020-09-04 00:51:39 +0300

    feat: implement brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    if (str.length % 2 !== 0) {
+        return false
+    }
+
+    let out=str.split('')
+    for (let i = 0; i < out.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (out[i] === bracketsConfig[j][0] && out[i + 1] === bracketsConfig[j][1]) {
+                out.splice(i, 2)
+                i-=2
+            }
+        }
+    }
+
+    return out.length===0
 }
